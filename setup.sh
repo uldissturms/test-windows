82277959c5fff58a18a8f00b9412abc738b5ecda
Uldis Sturms <uldis.sturms@gmail.com>
2015-05-12 23:09:48 +0100

    Started looking at running windows server tests against ec2 instance.

--- setup.sh
+++ setup.sh
@@ -10,4 +10,9 @@ cd windows
 make virtualbox/eval-win2012r2-standard # build virtualbox windows image
 vagrant box add windows-2012r2 ./box/virtualbox/eval-win2012r2-standard-nocm-1.0.4.box # register box with vagrant
 
+aws ec2 create-security-group --group-name "winrm group" --description "winrm group with enabled required incoming ports" # add ec2 winrm
+aws ec2 authorize-security-group-ingress --group-name "winrm group" --protocol tcp --port 80 --cidr "0.0.0.0/0" # enable http
+aws ec2 authorize-security-group-ingress --group-name "winrm group" --protocol tcp --port 3389 --cidr "0.0.0.0/0" # enable remote desktop
+aws ec2 authorize-security-group-ingress --group-name "winrm group" --protocol tcp --port 5985 --cidr "0.0.0.0/0" # enable winrm access to server
+
 
